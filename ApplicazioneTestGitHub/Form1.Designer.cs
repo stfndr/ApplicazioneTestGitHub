62bcbeebf1510bff1078b44e9adb60bb5d7a2a55
steand <steand@IABIT10072>
2022-11-09 18:26:26 +0100

    aggiunto un pulsante

--- ApplicazioneTestGitHub/Form1.Designer.cs
+++ ApplicazioneTestGitHub/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Bellissimo!!!";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,5 +67,6 @@
         #endregion
 
         private Label label1;
+        private Button button1;
     }
 }
