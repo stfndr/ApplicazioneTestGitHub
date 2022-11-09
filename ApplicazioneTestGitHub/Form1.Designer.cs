8b991ddb4c17d09b73c613e5b14fe75f087fe796
steand <steand@IABIT10072>
2022-11-09 18:52:10 +0100

    scaroco

--- ApplicazioneTestGitHub/Form1.Designer.cs
+++ ApplicazioneTestGitHub/Form1.Designer.cs
@@ -35,11 +35,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(126, 142);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.Size = new System.Drawing.Size(385, 89);
             this.label1.TabIndex = 0;
             this.label1.Text = "Bellissimo!!!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
--- ApplicazioneTestGitHub/Form1.cs
+++ ApplicazioneTestGitHub/Form1.cs
@@ -7,5 +7,9 @@ namespace ApplicazioneTestGitHub
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
